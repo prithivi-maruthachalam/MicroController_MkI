fe40727df9cc8a75205011d9041df2d502069f5d
prythagoras <prithivimaruthachalam@gmail.com>
2020-10-27 10:59:06 +0530

    Verified Data Mememory module

--- hdl/DMem.v
+++ hdl/DMem.v
@@ -14,7 +14,7 @@ module DMem(
 
     always @(posedge clk) begin
         if(E == 1 && WE == 1)           //Write the Data Input to Data Address
-            data_memory[Addr] <= DataIn 
+            data_memory[Addr] <= DataIn; 
     end
 
     assign DataOut = (E == 1) ? data_memory[Addr] : 0;  //If enabled put the value at Address in Data Out
